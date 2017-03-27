891a0403c946633ce9bedec4029ca06735986f5c
dnyanarth lonkar <dnyanarth.lonkar@gmail.com>
2017-03-27 23:57:46 +0530

    mailer config added for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   
+  config.action_mailer.default_url_options = {host: "https://easeinsurance.herokuapp.com/"}
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "imap.gmail.com",
+      :user_name            => "easeinsure88@gmail.com",
+      :password             => "8983491956",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }
 end
