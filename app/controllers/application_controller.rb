67de255930596f303fb82116c56db9f52ccf8080
dnyanarth lonkar <dnyanarth.lonkar@gmail.com>
2017-05-06 18:23:15 +0530

    agent nested form modification.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email,:user_type])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email,:user_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email,:user_type,:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email,:user_type, :username])
   end
 end
